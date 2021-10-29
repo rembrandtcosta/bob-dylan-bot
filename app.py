ad3601bf14c04b4a5398630007d739866a510207
rembrandtcosta <rembrandt.costa@ccc.ufcg.edu.br>
2021-10-29 20:54:55 -0300

    fix app.py

--- app.py
+++ app.py
@@ -1,23 +1,5 @@
-from flask import Flask
-from time import sleep
-from os import environ
-import update_bot
-
 while True: 
     update_bot.main()
     nap_time = 21601
     sleep(nap_time)
 
-'''
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    while True: 
-        update_bot.main()
-        nap_time = 21601
-        sleep(nap_time)
-
-if __name__ == "__main__":
-    app.run(port="5000")
-    '''
